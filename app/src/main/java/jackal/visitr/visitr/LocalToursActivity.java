197de851690883a827e3d10b14204dce31573110
albie <albertjbelardino@gmail.com>
2018-12-28 17:53:21 -0500

    added aws configuration

--- app/src/main/java/jackal/visitr/visitr/LocalToursActivity.java
+++ app/src/main/java/jackal/visitr/visitr/LocalToursActivity.java
@@ -2,6 +2,11 @@ package jackal.visitr.visitr;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
+
+import com.amazonaws.mobile.client.AWSMobileClient;
+import com.amazonaws.mobile.client.AWSStartupHandler;
+import com.amazonaws.mobile.client.AWSStartupResult;
 
 public class LocalToursActivity extends AppCompatActivity {
 
@@ -9,6 +14,13 @@ public class LocalToursActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_local_tours);
+
+        AWSMobileClient.getInstance().initialize(this, new AWSStartupHandler() {
+            @Override
+            public void onComplete(AWSStartupResult awsStartupResult) {
+                Log.d("YourMainActivity", "AWSMobileClient is instantiated and you are connected to AWS!");
+            }
+        }).execute();
 
         //key = AIzaSyCI80Oyo1ovK5M2gk-1WNUQdTvK6Plt3sw
 
