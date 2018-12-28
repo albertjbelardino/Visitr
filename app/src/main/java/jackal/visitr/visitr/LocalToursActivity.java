4e592d033d1268cbaf452d874702b9c8801ac4b3
unknown <albertjbelardino@gmail.com>
2018-12-28 14:51:42 -0500

    Initial commit

---
+++ app/src/main/java/jackal/visitr/visitr/LocalToursActivity.java
@@ -0,0 +1,13 @@
+package jackal.visitr.visitr;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class LocalToursActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_local_tours);
+    }
+}
