54282a196218ff05f7fa32a18acad500ce663ddc
Jakk-Git <JacksonPotash@gmail.com>
2018-12-31 13:51:35 -0500

    Added main activities.

--- app/src/main/java/jackal/visitr/visitr/LocalToursActivity.java
+++ app/src/main/java/jackal/visitr/visitr/LocalToursActivity.java
@@ -1,7 +1,7 @@
 package jackal.visitr.visitr;
 
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
 import android.util.Log;
 
 import com.amazonaws.mobile.client.AWSMobileClient;
