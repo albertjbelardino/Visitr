db0cfcbf2f4a0d35b8328872f925a7a67d5da4bc
albie <albertjbelardino@gmail.com>
2018-12-31 19:08:55 -0500

    did a test post to the database, added a single review to the reviews table, put junk in create a table activity

--- app/src/main/java/jackal/visitr/visitr/CreateTourActivity.java
+++ app/src/main/java/jackal/visitr/visitr/CreateTourActivity.java
@@ -4,18 +4,81 @@ import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.support.v7.widget.Toolbar;
+import android.util.Log;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+
+import com.amazonaws.auth.AWSCredentialsProvider;
+import com.amazonaws.mobile.client.AWSMobileClient;
+import com.amazonaws.mobile.client.AWSStartupHandler;
+import com.amazonaws.mobile.client.AWSStartupResult;
+import com.amazonaws.mobile.config.AWSConfiguration;
+import com.amazonaws.mobileconnectors.dynamodbv2.dynamodbmapper.DynamoDBMapper;
+import com.amazonaws.services.dynamodbv2.AmazonDynamoDBClient;
+
+import Models.IndividualReviewDO;
 
 public class CreateTourActivity extends AppCompatActivity {
+
+    DynamoDBMapper dynamoDBMapper;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_create_tour);
 
+        AWSMobileClient.getInstance().initialize(this, new AWSStartupHandler() {
+            @Override
+            public void onComplete(AWSStartupResult awsStartupResult) {
+                Log.d("YourMainActivity", "AWSMobileClient is instantiated and you are connected to AWS!");
+            }
+        }).execute();
+
+        AWSMobileClient.getInstance().initialize(this).execute();
+
+        AWSCredentialsProvider credentialsProvider = AWSMobileClient.getInstance().getCredentialsProvider();
+        AWSConfiguration configuration = AWSMobileClient.getInstance().getConfiguration();
+
+
+        // Add code to instantiate a AmazonDynamoDBClient
+        AmazonDynamoDBClient dynamoDBClient = new AmazonDynamoDBClient(credentialsProvider);
+
+        this.dynamoDBMapper = DynamoDBMapper.builder()
+                .dynamoDBClient(dynamoDBClient)
+                .awsConfiguration(configuration)
+                .build();
+
         Toolbar menuToolbar = (Toolbar) findViewById(R.id.menu_toolbar);
         setSupportActionBar(menuToolbar);
+
+        Button button = (Button) findViewById(R.id.button);
+
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                createIndividualReview();
+            }
+        });
+    }
+
+    public void createIndividualReview() {
+        final IndividualReviewDO individualReviewItem = new IndividualReviewDO();
+
+        individualReviewItem.set_id(Integer.parseInt(((EditText) findViewById(R.id._id)).getText().toString()));
+        individualReviewItem.set_tour_id(Integer.parseInt(((EditText) findViewById(R.id._id)).getText().toString()));
+        individualReviewItem.set_creator_id(((EditText) findViewById(R.id._creator_id)).getText().toString());
+        individualReviewItem.set_content(((EditText) findViewById(R.id._content)).getText().toString());
+
+        new Thread(new Runnable() {
+            @Override
+            public void run() {
+                dynamoDBMapper.save(individualReviewItem);
+                // Item saved
+            }
+        }).start();
     }
 
     @Override
